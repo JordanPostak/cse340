02b5afcc62a2646ad12e3595d31f7750ba31aa95
JordanPostak <jordan_postak@hotmail.com>
2023-10-11 10:44:59 -0600

    registration and controller added

--- view/registration.php
+++ view/registration.php
@@ -17,21 +17,31 @@
         </nav>
         <main>
             <h1>Register</h1>
+
+            <?php
+            if (isset($message)) {
+                echo $message;
+                }
+                ?>
+                <form method="post" action="/phpmotors/accounts/index.php">
+                    
             <form action="/phpmotors/accounts/index.php" method="post">
                 <label for="firstName">First Name:</label>
-                <input type="text" id="firstName" name="firstName" required>
+                <input type="text" id="firstName" name="clientFirstname" required>
 
                 <label for="lastName">Last Name:</label>
-                <input type="text" id="lastName" name="lastName" required>
+                <input type="text" id="lastName" name="clientLastname" required>
 
                 <label for="email">Email Address:</label>
-                <input type="email" id="email" name="email" required>
+                <input type="email" id="email" name="clientEmail" required>
 
                 <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
+                <input type="password" id="password" name="clientPassword" required>
                 <h2>Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</h2>
 
                 <button type="submit">Register</button>
+                <!-- Add the action name - value pair -->
+                <input type="hidden" name="action" value="register">
             </form>
         </main>
         <footer>
