39b98b64a29c27300d8e9bd84d7664b3575ba7d4
JordanPostak <jordan_postak@hotmail.com>
2023-11-23 22:28:00 -0700

    enhansment 9 done

--- view/image-admin.php
+++ view/image-admin.php
@@ -23,41 +23,39 @@ if (isset($_SESSION['message'])) {
         <nav>
             <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/navigation.php'; ?> 
         </nav>
-        <main>
-        <h1>Image Management</h1>
-        <p>Welcome to the image management page. Please choose one of the options presented below.</p>
+        <main class="image-admin">
+            <h1>Image Management</h1>
+            <p>Welcome to the image management page. Please choose one of the options presented below.</p>
 
-        <h2>Add New Vehicle Image</h2>
-        <?php
-        if (isset($message)) {
-            echo $message;
-        }
-        ?>
+            <h2>Add New Vehicle Image</h2>
+            <?php
+            if (isset($message)) {
+                echo $message;
+            }
+            ?>
 
-        <form action="/phpmotors/uploads/" method="post" enctype="multipart/form-data">
-            <label for="invItem">Vehicle</label>
-            <?php echo $prodSelect; ?>
-            <fieldset>
-                <label>Is this the main image for the vehicle?</label>
-                <label for="priYes" class="pImage">Yes</label>
-                <input type="radio" name="imgPrimary" id="priYes" class="pImage" value="1">
-                <label for="priNo" class="pImage">No</label>
-                <input type="radio" name="imgPrimary" id="priNo" class="pImage" checked value="0">
-            </fieldset>
-            <label>Upload Image:</label>
-            <input type="file" name="file1">
-            <input type="submit" class="regbtn" value="Upload">
-            <input type="hidden" name="action" value="upload">
-        </form>
-        <hr>
-
-        <h2>Existing Images</h2>
-        <p class="notice">If deleting an image, delete the thumbnail too and vice versa.</p>
-        <?php
-        if (isset($imageDisplay)) {
-            echo $imageDisplay;
-        }
-        ?>
+            <form action="/phpmotors/uploads/" method="post" enctype="multipart/form-data">
+                <?php echo $prodSelect; ?>
+                <fieldset>
+                    <label>Is this the main image for the vehicle?</label>
+                    <label for="priYes" class="pImage">Yes</label>
+                    <input type="radio" name="imgPrimary" id="priYes" class="pImage" value="1">
+                    <label for="priNo" class="pImage">No</label>
+                    <input type="radio" name="imgPrimary" id="priNo" class="pImage" checked value="0">
+                </fieldset>
+                <label for="file1">Upload Image:</label>
+                <input class="file-btn" type="file" name="file1" id="file1">
+                <input type="submit" class="upload-button" value="Upload">
+                <input type="hidden" name="action" value="upload">
+            </form>
+            <hr>
+            <h2>Existing Images</h2>
+            <p class="notice">If deleting an image, delete the thumbnail too and vice versa.</p>
+            <?php
+            if (isset($imageDisplay)) {
+                echo $imageDisplay;
+            }
+            ?>
         </main>
         <footer>
             <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/footer.php'; ?>
